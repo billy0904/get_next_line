3dcf228149d18fe55aacf80b49a1135ee1cf65f8
billy <gb94@ewhain.net>
2023-12-04 15:45:49 +0900

    gitignore test

--- get_next_line.h
+++ get_next_line.h
@@ -11,14 +11,16 @@
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include <stdlib.h>
+# include <unistd.h>
+# include <stdlib.h>
 
-#ifndef BUFF_SIZE
-#define BUFFER_SIZE 1024
-#endif
+# ifndef BUFF_SIZE
+#  define BUFFER_SIZE 1024
+# endif
 
-char *get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*get_next_line(int fd);
 
 #endif
