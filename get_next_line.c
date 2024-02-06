30266056abeada3cfc33460d8ae24b7579c2c5bf
billy <gb94@ewhain.net>
2024-02-06 22:25:45 +0900

    0206

--- get_next_line.c
+++ get_next_line.c
@@ -12,9 +12,29 @@
 
 #include "get_next_line.h"
 
+char	*split(char *str)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	while(str[i] == "\n")
+	{
+		i++;
+	}
+}
+
 char	*get_next_line(int fd)
 {
-	char	*line;
+	char		*line;
+	char		*buffer;
+	static char	*save;
 
+	if (save 안에 내용이 있을 경우)
+		return (save);
+	while (1)
+	{
+		line = read(fd, buffer, BUFFER_SIZE);
+	}
 	return (line);
 }
